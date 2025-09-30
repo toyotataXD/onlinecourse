3e4688dacc1a8da8a43667ecc2c2161cbec20c31
toyotata <kilicataalp@gmail.com>
2025-09-30 19:54:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-import time
-meme_dictionary = {
-            "CRINGE": "Garip ya da utandırıcı bir şey",
-            "cringe": "Garip ya da utandırıcı bir şey",
-            "LOL": "Komik bir şeye verilen cevap",
-            "lol": "Komik bir şeye verilen cevap",
-            "ROFL": "bir şakaya karşılık cevap",
-            "rofl": "bir şakaya karşılık cevap",
-            "SHEESH": "onaylamak",
-            "sheesh": "onaylamak",
-            "CREEPY": "korkunç",
-            "creepy": "korkunç",
-            "AGGRO ": "agresifleşmek/sinirlenmek",
-            "aggro ": "agresifleşmek/sinirlenmek",
-            }
-
-
-word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
-
-
-if word in meme_dictionary.keys():
-    meaning = meme_dictionary[word]
-    print("Anlamı:", meaning)
+while True:
+    meme_dictionary = {
+                "CRINGE": "Garip ya da utandırıcı bir şey",
+                "cringe": "Garip ya da utandırıcı bir şey",
+                "LOL": "Komik bir şeye verilen cevap",
+                "lol": "Komik bir şeye verilen cevap",
+                "ROFL": "bir şakaya karşılık cevap",
+                "rofl": "bir şakaya karşılık cevap",
+                "SHEESH": "onaylamak",
+                "sheesh": "onaylamak",
+                "CREEPY": "korkunç",
+                "creepy": "korkunç",
+                "AGGRO ": "agresifleşmek/sinirlenmek",
+                "aggro ": "agresifleşmek/sinirlenmek",
+                }
     
     
-
-      
-else:
-    print("Sözlükte böyle bir kelime yok.")
+    word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+    
+    
+    if word in meme_dictionary.keys():
+        meaning = meme_dictionary[word]
+        print("Anlamı:", meaning)
+        
+        
+    
+          
+    else:
+        print("Sözlükte böyle bir kelime yok.")
